1cfae08c380a4c7f5fbf3cbbcbe692b2e348eff8
CytFree <1154557052@qq.com>
2017-12-02 12:09:56 +0800

    类、方法添加注释

--- src/main/java/com/test/CxfServiceImpl.java
+++ src/main/java/com/test/CxfServiceImpl.java
@@ -2,9 +2,19 @@ package com.test;
 
 import javax.jws.WebService;
 
+/**
+ * @author cyt19930705
+ * @date 2017/10/11
+ */
 @WebService()
 public class CxfServiceImpl implements CxfService{
 
+	/**
+	 * helloWorld
+	 * @param name
+	 * @return
+	 */
+	@Override
 	public String helloWorld(String name) {
 		// TODO Auto-generated method stub
 		return "Hello World " + name;
